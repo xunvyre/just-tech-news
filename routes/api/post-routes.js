60e003534326855e1138479cacba63dc34fb3ef4
xunvyre <victoria@thedavids.us>
2022-09-06 19:21:29 -0400

    refactor

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -83,26 +83,8 @@ router.post('/', (req, res) =>
 //upvoting a post
 router.put('/upvote', (req, res) =>
 {
-    Vote.create
-    ({
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-    .then(() =>
-    {
-        return Post.findOne
-        ({
-          where: {id: req.body.post_id},
-          attributes:
-          [
-            'id', 'post_url', 'title', 'created_at',
-            [
-              sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-              'vote_count'
-            ]
-          ]
-        })
-    })
+    //static method created in models/Post.js
+    Post.upvote(req.body, {Vote})
     .then(dbPostData => res.json(dbPostData))
     .catch(err =>
     {
