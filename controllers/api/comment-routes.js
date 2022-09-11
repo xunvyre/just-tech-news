4e125ea0ee3ea16da8dd1f692910633cec1545f0
xunvyre <victoria@thedavids.us>
2022-09-11 15:54:05 -0400

    added conditions based on sessions

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -8,18 +8,23 @@ router.get('/', (req, res) =>
 
 router.post('/', (req, res) =>
 {
-    Comment.create
-    ({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-    .then (dbCommentData => res.json(dbCommentData))
-    .catch(err =>
+    //check session
+    if (req.session)
     {
-        console.log(err);
-        res.status(400).json(err);
-    });
+        Comment.create
+        ({
+            comment_text: req.body.comment_text,
+            //use user id from session
+            user_id: req.session.user_id,
+            post_id: req.body.post_id
+        })
+        .then (dbCommentData => res.json(dbCommentData))
+        .catch(err =>
+        {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
 });
 
 router.delete('/:id', (req, res) =>
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,7 +33,7 @@ router.get('/', (req, res) =>
     {
         //pass a single post object into the homepage template
         const posts = dbPostData.map(post => post.get({plain: true}));
-        res.render('homepage', {posts});
+        res.render('homepage', {posts, loggedin: req.session.loggedin});
     })
     .catch(err =>
     {
@@ -90,7 +90,7 @@ router.get('/post/:id', (req, res) =>
         //serialize the data
         const post = dbPostData.get({plain: true});
         //pass data to template
-        res.render('single-post', {post});
+        res.render('single-post', {post, loggedin: req.session.loggedin});
     })
     .catch(err =>
     {
